243f5ac474c6402c67a33efe4acb750513b42dba
sp <Guest@epicodus.local>
2017-03-31 11:15:25 -0700

    Add UI logic to project, and print results to page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,56 @@
 $(document).ready(function(){
 
-  var input = "sowmyaisastudentatepicodus";
-  var message = input.split("");
-  var messageLength = message.length;
-  var scramMessage = [];
-  // write some math to determine next largest square root
-  var root = Math.sqrt(messageLength);
-  var roundRoot = Math.ceil(root);
-  console.log(roundRoot);
-  // roundRoot is our number of cols and rows
+  $("form").submit(function(e) {
+    e.preventDefault();
+    // collect input from user
+    var input = $("form input").val();
+    // remove spaces, and special chars
+    modifiedInput = input.replace(/ /g,"");
+    // remove all special chars
+    modifiedInput = modifiedInput.replace(/[^\w\s]/gi, '');
+    // downcase all chars in input string
+    modifiedInput = modifiedInput.toLowerCase();
+    var message = modifiedInput.split("");
+    var messageLength = message.length;
+    var scramWord = [];
+    var scramMessage = [];
+    // write some math to determine next largest square root
+    var root = Math.sqrt(messageLength);
+    var roundRoot = Math.ceil(root);
+    console.log(roundRoot);
+    // roundRoot is our number of cols and rows
 
-  var writeColumn = function(i) {
-    // var i = 0;
-    var count = 1;
-    while (count <= roundRoot) {
-      var letter = message[i];
-      scramMessage.push(letter);
-      i = i + roundRoot;
-      count++;
+    var writeColumn = function(i) {
+      var count = 1;
+      while (count <= roundRoot) {
+        var letter = message[i];
+        scramWord.push(letter);
+        i = i + roundRoot;
+        count++;
+      }
+      return scramWord.join("");
     }
-    scramMessage.push(" ");
-    return scramMessage.join("");
-  }
 
-  for(var i = 0; i < roundRoot; i++){
-    console.log(writeColumn(i));
-  }
+    for(var i = 0; i < roundRoot; i++){
+      scramMessage.push(writeColumn(i));
+    }
+    var lastMessage = scramMessage.length - 1;
+    var encodedMessage = scramMessage[lastMessage].split("");
+    var encodedLength = encodedMessage.length;
+    // loop through encoded message and splice a space in every after 5 chars
+    var n = 5;
+    while (n <= (encodedLength + 5)) {
+      encodedMessage.splice(n, 0, " ");
+      n = n + 6;
+    }
 
+    $("#initial-input").text('"' + input + '" becomes:');
+    $("#encoded-message").text(encodedMessage.join(""));
+  });
 });
+
+// dev's notes: I have not successfully sepparated the buisiness logic and UI logic for this project.
+// Everytime I try to sepparate the two, I start receiving unsolveable console errors.
+// Sometime soon I'd like to achive this sepparation
+
+// end
